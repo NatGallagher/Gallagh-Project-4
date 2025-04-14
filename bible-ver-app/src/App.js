bdf7c781ba08197330541a03443e3ea78083d4e3
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-14 11:52:18 -0400

    login.js linked

--- bible-ver-app/src/App.js
+++ bible-ver-app/src/App.js
@@ -1,18 +1,14 @@
-
+import {HashRouter, Routes, Route} from "react-router-dom"
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
-      <div>
-        <h2>Daily Encouragement</h2>
-        <h4>Login</h4>
-        <form>
-                <label>Username: </label><input type="text" placeholder="* username" maxLength={25}></input><br/>
-                <label>Password: </label><input type="password" placeholder="* password" maxLength={25}></input>
-                <p></p>
-                <button>Login</button>
-            </form>
-      </div>
+       <HashRouter>       
+      <Routes>      
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+    </HashRouter>
     </>
   );
 }
