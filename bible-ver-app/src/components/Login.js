3140ecd9f62d3829480e4cf0d9bcfd4b57255924
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-14 11:50:12 -0400

    login.js created inside of componenets

---
+++ bible-ver-app/src/components/Login.js
@@ -0,0 +1,18 @@
+function Login() {
+    return (
+      <>
+        <div>
+          <h2>Daily Encouragement</h2>
+          <h4>Login</h4>
+          <form>
+                  <label>Username: </label><input type="text" placeholder="* username" maxLength={25}></input><br/>
+                  <label>Password: </label><input type="password" placeholder="* password" maxLength={25}></input>
+                  <p></p>
+                  <button>Login</button>
+              </form>
+        </div>
+      </>
+    );
+  }
+  
+  export default Login;
