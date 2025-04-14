20dd6004df3a630a2d4dad3cc1df4e5ccc4a2dee
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-14 11:45:57 -0400

    login page created

--- bible-ver-app/src/App.js
+++ bible-ver-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Live update ...1</p>
-      </header>
-    </div>
+    <>
+      <div>
+        <h2>Daily Encouragement</h2>
+        <h4>Login</h4>
+        <form>
+                <label>Username: </label><input type="text" placeholder="* username" maxLength={25}></input><br/>
+                <label>Password: </label><input type="password" placeholder="* password" maxLength={25}></input>
+                <p></p>
+                <button>Login</button>
+            </form>
+      </div>
+    </>
   );
 }
 
