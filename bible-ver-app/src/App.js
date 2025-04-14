922ef9ae654bc60c32cae75deb5200913a4768ff
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-14 12:01:21 -0400

    register.js added and added to route

--- bible-ver-app/src/App.js
+++ bible-ver-app/src/App.js
@@ -1,5 +1,6 @@
 import {HashRouter, Routes, Route} from "react-router-dom"
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
        <HashRouter>       
       <Routes>      
         <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>      
       </Routes>
     </HashRouter>
     </>
---
+++ bible-ver-app/src/components/Register.js
@@ -0,0 +1,20 @@
+function Register() {
+    return (
+      <>
+        <div>
+          <h2>Daily Encouragement</h2>
+          <h4>Register</h4>
+          <form>
+                  <label>Username: </label><input type="text" placeholder="* username" maxLength={25}></input><br/>
+                  <label>Confirm Username: </label><input type="text" placeholder="* confirm username" maxLength={25}></input><br/>
+                  <label>Password: </label><input type="password" placeholder="* password" maxLength={25}></input><br/>
+                  <label>Confirm Password: </label><input type="password" placeholder="* confirm password" maxLength={25}></input>
+                  <p></p>
+                  <button>Submit</button>
+              </form>
+        </div>
+      </>
+    );
+  }
+  
+  export default Register;
